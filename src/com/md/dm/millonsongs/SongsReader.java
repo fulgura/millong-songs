6156e079b8e712b4f9bca2476e1d49a0d3e9dbbc
Diego Martín Correa <diegomartincorrea@gmail.com>
2012-10-21 23:33:19 -0300

    reading hdf5 files from millon songs

---
+++ src/com/md/dm/millonsongs/SongsReader.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.md.dm.millonsongs;
+
+/**
+ * @author dicorrea
+ *
+ */
+public class SongsReader {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+		
+
+	}
+
+}
